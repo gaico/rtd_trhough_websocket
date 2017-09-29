db450ee2ff85b46d7fb9a44fdeead316611b8b2f
Gaico Stoltenborgh <gaico.stoltenborgh@rabobank.nl>
2017-09-29 16:01:07 +0200

    first commit

---
+++ build.bat
@@ -0,0 +1,3 @@
+set JAVA_HOME=E:\java\jdk1.8.0_112
+set PATH=%PATH%;%JAVA_HOME%:
+mvn clean install
---
+++ run.bat
@@ -0,0 +1,2 @@
+set JRE_HOME=E:\java\jre1.8.0_112
+java -jar -Dserver.port=9090 target\rtd_through_websocket-0.0.1-SNAPSHOT.jar 
